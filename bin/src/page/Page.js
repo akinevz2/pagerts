86c0de1808bfb5feb007ef75c25882776d500fa0
Kirill kine Nevzorov <akinevz@outlook.com>
2025-07-22 07:11:32 +0000

    cleanup

--- bin/src/page/Page.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isPage = exports.isError = void 0;
-const isError = (page) => 'error' in page;
-exports.isError = isError;
-const isPage = (page) => "resources" in page && Array.isArray(page.resources);
-exports.isPage = isPage;
-//# sourceMappingURL=Page.js.map
--- bin/src/printers/AbstractResourcePrinter.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AbstractResourcePrinter = void 0;
-class AbstractResourcePrinter {
-    constructor() { }
-}
-exports.AbstractResourcePrinter = AbstractResourcePrinter;
-//# sourceMappingURL=AbstractResourcePrinter.js.map
